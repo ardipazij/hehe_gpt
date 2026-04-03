c2d038417dd4fae5cabbd5db6ed375b35da2576a
ardipazij <121768780+ardipazij@users.noreply.github.com>
2026-04-03 15:22:14 +0300

    feat():  impl FeadForward class

---
+++ ffn.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+class FeedForward(nn.Module):
+    def __init__(self, emb_size : int, dropout = 0.1):
+        super().__init__()
+        
+        self.emb_size = emb_size
+        
+        self.first_linear = nn.Linear(self.emb_size, self.emb_size * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.second_linear = nn.Linear(self.emb_size * 4, self.emb_size)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x : torch.Tensor):
+        first_iter = self.first_linear(x)
+        func_act = self.relu(first_iter)
+        second_iter = self.second_linear(func_act)
+        
+        return self.dropout(second_iter)
