798b3e128e9b845fd7754d1a20e14df0975a09e4
ardipazij <121768780+ardipazij@users.noreply.github.com>
2026-04-03 03:30:36 +0300

    feat(): add PositionalEmbeddings class

---
+++ positional_embeddings.py
@@ -0,0 +1,10 @@
+from torch import nn
+class PositionalEmbeddings(nn.Module):
+    def __init__(self, vocab_size : int, emb_size : int):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.emb_size = emb_size
+        self.embedding = nn.Embedding(self.vocab_size, self.emb_size)
+        
+    def forward(self, seq_len : int):
+         return self.embedding(torch.Tensor([i for i in range(seq_len)]).long())
