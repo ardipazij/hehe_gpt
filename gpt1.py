74ce62482e4885b85900063abec4408ae79c2e18
ardipazij <121768780+ardipazij@users.noreply.github.com>
2026-04-06 23:15:02 +0300

    :sparkles: feat(): add DataLoader and impl fit and autoregressive function :sparkles:

--- gpt1.py
+++ gpt1.py
@@ -1,5 +1,22 @@
 from torch import nn
 from typing import List
+from torch.utils.data import Dataset
+
+class GetData(Dataset):
+    def __init__(self, data: list, seq_len: int, device: str):
+        self.data = torch.tensor(data, dtype=torch.long, device=device)
+        self.seq_len = seq_len
+        self.device = device
+
+    def __len__(self):
+        return len(self.data) - self.seq_len - 1
+
+    def __getitem__(self, idx: int):
+        x = self.data[idx : idx + self.seq_len]
+        y = self.data[idx + 1 : idx + 1 + self.seq_len]
+        
+        return x, y
+    
 class TokenEmbeddings(nn.Module):
     def __init__(self, vocab_size : int, emb_size : int):
         super().__init__()
@@ -166,7 +183,7 @@ class GPT(nn.Module):
             logits = self.linear(res_drop)
             
             return logits
-    def generate(self, x : torch.Tensor, max_new_tokens : int):
+    def generate(self, x : torch.Tensor, max_new_tokens : int, do_sample : bool, top_k : int, top_p : float,temperature = 1.0):
         '''
         Начните цикл до max_new_tokens:
 Обрежьте поступившую последовательность до максимальной возможной длины. Для этого возьмите последние max_seq_len токенов в последовательности.
@@ -177,44 +194,64 @@ class GPT(nn.Module):
 '''
         for _ in range(max_new_tokens):
             x_cond = x[:, -self.max_seq_len:]
-            logits = self.forward(x_cond)
             
-            #Берем логиты только для последнего временного шага 
-            last_logits = logits[:, -1, :]
+            logits = self.forward(x_cond)[:, -1, :] / temperature
             
-            probs = torch.softmax(last_logits, dim=-1)
-            next_token = torch.argmax(probs, dim=-1, keepdim=True) # (batch_size, 1)
+            if do_sample:
+                if top_k is not None:
+                    v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                    logits[logits < v[:, [-1]]] = float('-inf')
+                
+                if top_p is not None:
+                    probs = torch.softmax(logits, dim=-1)
+                    sorted_probs, sorted_indices = torch.sort(probs, descending=True, dim=-1)
+                    cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+                    keep_sorted = cumulative_probs < top_p
+                    keep_sorted[:, 0] = 1 # Всегда оставляем токен с мин. кум. вероятностью
+                    
+                    top_p_mask = torch.zeros_like(keep_sorted, dtype=torch.uint8)
+                    top_p_mask.scatter_(dim=-1, index=sorted_indices, src=keep_sorted.byte())
+                    
+                    logits[top_p_mask == 0] = float('-inf')
+
+                probs = torch.softmax(logits, dim=-1)
+                next_token = torch.multinomial(probs, num_samples=1)
+            else:
+                next_token = torch.argmax(logits, dim=-1, keepdim=True)
             
             x = torch.cat((x, next_token), dim=1)
             
         return x
-    def save(self, path):
-      torch.save({
-          'model_state_dict': self.state_dict(),
-          'vocab_size': self.vocab_size,
-          'max_seq_len': self.max_seq_len,
-          'emb_size': self.emb_size,
-          'num_heads': self.num_heads,
-          'head_size': self.head_size,
-          'num_layers': self.num_layers
-      }, path)
-      
-  @classmethod
-  def load(cls, path, device):
-      checkpoint = torch.load(path, map_location=device)
-      model = cls(
-          vocab_size=checkpoint['vocab_size'],
-          max_seq_len=checkpoint['max_seq_len'],
-          emb_size=checkpoint['emb_size'],
-          num_heads=checkpoint['num_heads'],
-          head_size=checkpoint['head_size'],
-          num_layers=checkpoint['num_layers']
-      )
-      model.load_state_dict(checkpoint['model_state_dict'])
-      model.to(device)
-      return model
-        
+    def fit(self, train_loader, valid_loader, num_epoch: int, learning_rate: float):
+        self.to(self.device)
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
 
+        for epoch in range(num_epoch):
+            self.train()
+            for inputs, targets in train_loader:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
 
+                logits = self.forward(inputs) 
+                
+                logits_flat = logits.view(-1, logits.size(-1))
+                targets_flat = targets.view(-1)
 
+                loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+                self.train_loss = loss 
 
+                optimizer.zero_grad()
+                self.train_loss.backward()
+                optimizer.step()
+
+            self.eval()
+            with torch.no_grad():
+                for inputs, targets in valid_loader:
+                    inputs, targets = inputs.to(self.device), targets.to(self.device)
+
+                    logits = self.forward(inputs)
+                    logits_flat = logits.view(-1, logits.size(-1))
+                    targets_flat = targets.view(-1)
+
+                    v_loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+                    self.valid_loss = v_loss
