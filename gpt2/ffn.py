a04c2646ab280af2acd818ce82797b27339555be
ardipazij <teddy.remus.edward.lupin@gmail.com>
2026-04-07 23:04:42 +0300

    feat(gpt2): modified ffn with gelu

---
+++ gpt2/ffn.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from gelu import GELU
+
+
+class FeedForward(nn.Module):
+    def __init__(self, emb_size: int, dropout=0.1):
+        super().__init__()
+
+        self.emb_size = emb_size
+
+        self.first_linear = nn.Linear(self.emb_size, self.emb_size * 4)
+        self.gelu = GELU()
+        self.second_linear = nn.Linear(self.emb_size * 4, self.emb_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor):
+        first_iter = self.first_linear(x)
+        func_act = self.gelu(first_iter)
+        second_iter = self.second_linear(func_act)
+
+        return self.dropout(second_iter)
